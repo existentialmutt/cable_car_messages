ff49ea27ed2b6f1f8199c5e769085591001dacba
Rafe Rosen <rafe@existentialmutt.com>
2021-06-08 21:17:44 -0400

    initial commit, install dependencies

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,18 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Turbolinks from "turbolinks"
+Turbolinks.start()
+
+import mrujs from "mrujs";
+mrujs.start();
+
+// stimulus
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
